3e261b4735c3e5fa294752110e72e66bde204e79
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-02-01 11:39:53 -0700

    video preview done

--- includes/header.php
+++ includes/header.php
@@ -14,6 +14,10 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
     <head>
         <title>Welcome to Channel 9</title>
         <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/06a651c8da.js" crossorigin="anonymous"></script>
+        <script src="assets/js/script.js"></script>
     </head>
     <body>
         <div class='wrapper'>
