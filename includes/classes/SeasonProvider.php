d368881d2873c98cda7f5737cd7bf0bc17bad45b
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-02-02 18:56:56 -0700

    you might also like logic

--- includes/classes/SeasonProvider.php
+++ includes/classes/SeasonProvider.php
@@ -55,7 +55,7 @@ class SeasonProvider {
                             <img src='$thumbnail'>
 
                             <div class='videoInfo'>
-                                <h4>$name</h4>
+                                <h4>$episodeNumber. $name</h4>
                                 <span>$description</span>
                             </div>
 
